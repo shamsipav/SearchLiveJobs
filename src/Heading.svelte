8ec5240c65421c483c95ddc6fb684b37bbc0cb67
shamsipav <aseevpav@gmail.com>
2022-07-20 13:25:34 +0500

    Types for Heading

--- src/Heading.svelte
+++ src/Heading.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     export let size: number;
-    const options = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-    let selected = options[size - 1];
+    const options: string[] = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    let selected: string = options[size - 1];
 </script>
 
 <svelte:element this={selected}>
